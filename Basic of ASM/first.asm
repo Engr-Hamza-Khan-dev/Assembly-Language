8014729e9b248bf3b39ec12ee0f74d4f69ebdd76
Muhammad Hamza Khan <hamzuu6971@gmail.com>
2026-02-11 22:46:44 +0500

    updated

--- Basic of ASM/first.asm
+++ Basic of ASM/first.asm
@@ -1,18 +1,26 @@
 org 100h
 
-mov ax,4
-add ax,4
-add ax,4
-add ax,4
+;mov ax,4
+;add ax,4      
+;add ax,4
+;add ax,4
 
+;mov bx,3
+;add bx,3
+;add bx,3
+
+;add ax,bx  
+
+mov ax,3
+add ax,1
+mov ax,2
 mov bx,3
-add bx,3
-add bx,3
-
+sub bx,ax
 add ax,bx
 
 mov ax,0x4c00
 int  0x21
+
 
 ; Run Commands:
 ; nasm -f win64 hello.asm -o hello.obj
