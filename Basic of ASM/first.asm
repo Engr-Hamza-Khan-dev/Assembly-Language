a5f5f36dc0faa7323948e0159d33fbbe5e119f0b
Muhammad Hamza Khan <hamzuu6971@gmail.com>
2026-02-11 22:10:01 +0500

    first commit

---
+++ Basic of ASM/first.asm
@@ -0,0 +1,20 @@
+org 100h
+
+mov ax,4
+add ax,4
+add ax,4
+add ax,4
+
+mov bx,3
+add bx,3
+add bx,3
+
+add ax,bx
+
+mov ax,0x4c00
+int  0x21
+
+; Run Commands:
+; nasm -f win64 hello.asm -o hello.obj
+; gcc hello.obj -o hello.exe
+; hello
